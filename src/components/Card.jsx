f8c3fe6111321449cb844edd2f8f25aba2167418
Juan Blanco <juanblancodev@gmail.com>
2024-04-21 20:18:15 -0700

    footer added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types'
 
 import Header from './Header'
 import Info from './Info'
+import Footer from './Footer'
 
 const Wrapper = styled.section`
   max-width: 320px;
   width: 100%;
-  height: 500px;
+  height: 530px;
 
   margin: 60px auto;
   padding: 25px;
@@ -27,6 +28,7 @@ const Card = ({ info }) => {
       <Header image={info.image} name={info.name} id={info.id} />
       <Info description={info.description} value={info.value} daysLeft={info.daysLeft} />
       <Hr />
+      <Footer firstName={info.owner.firstName} lastName={info.owner.lastName} avatar={info.owner.avatar} />
     </Wrapper>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Avatar = styled.img`
+  width: 35px;
+  height: 35px;
+  border: 1px solid var(--color-white);
+  border-radius: 50%;
+`
+
+const Footer = ({ firstName, lastName, avatar }) => {
+  return (
+    <footer className='flex align-center gap-3'>
+      <Avatar src={`assets/avatar/${avatar}`} alt="" />
+      <p className='primary'>
+        Creation of <span className='white'>{firstName} {lastName}</span>
+      </p>
+    </footer>
+  )
+}
+
+Footer.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired
+}
+
+export default Footer
